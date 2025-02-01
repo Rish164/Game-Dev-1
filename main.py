2af9c3ab7a2b4028c3ca12bba31b24e3897c74d0
Rish164 <risheekverma25@gmail.com>
2025-02-02 03:50:10 +0530

    Changed how the quit works.

--- main.py
+++ main.py
@@ -21,8 +21,16 @@ while running:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
             running = False
-            sys.exit()
+            #sys.exit(): Removing this now, because this instantly kills the program
+            #and we want to close the window first and then kill the program
 
     pygame.display.update()
+
+# Quit the game: Outside the event loop
+# If pygame.quit() is inside the loop, 
+# it will execute as soon as the loop condition is met (when running = False). 
+# However, at that point: The Pygame window will close immediately, preventing
+# any remaining events or updates from completing cleanly. This can cause 
+# unnecessary delays or issues when you're wrapping up the game.
+pygame.quit()
