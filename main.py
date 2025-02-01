550b16d3da2ed1c790528e34837c13af90c5ebf1
Rish164 <risheekverma25@gmail.com>
2025-02-02 03:30:58 +0530

    Set window icon and caption.

--- main.py
+++ main.py
@@ -9,3 +9,20 @@ pygame.init()
 W = 800
 H = 600
 screen = pygame.display.set_mode((W, H))
+pygame.display.set_caption("Space Invaders: The Game")
+pygame.display.set_icon(pygame.image.load('Game-Dev-1/images/gameicon.png'))
+
+#Now we will start the Event/Game loop
+running = True
+#EventLoop
+while running:
+
+    screen.fill((0, 0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            running = False
+            sys.exit()
+
+    pygame.display.update()
