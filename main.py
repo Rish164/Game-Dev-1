d5da3cf86cf82c56870dca346a308455097de00a
Rish164 <risheekverma25@gmail.com>
2025-02-02 05:11:52 +0530

    Added Player, and end game boundary.

--- main.py
+++ main.py
@@ -11,6 +11,30 @@ screen = pygame.display.set_mode((W, H))
 pygame.display.set_caption("Space Invaders: The Game")
 pygame.display.set_icon(pygame.image.load('Game-Dev-1/images/gameicon.png'))
 
+#Player
+playerImg = pygame.image.load('Game-Dev-1/images/spaceship.png')
+playerX = W/2 - 32
+playerY = 4*H/5
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+#End_line/Asteroid Belt
+rockImg = pygame.image.load('Game-Dev-1/images/stone.png')
+rWidth = 24
+rHeight = 24
+beltY = playerY + 32
+#..........................................................................
+num_of_rocks = W//rWidth
+remaining_space = W - (num_of_rocks*rWidth)
+extra_space_per_stone = remaining_space//(num_of_rocks - 1) if num_of_rocks > 1 else 0
+
+def draw_belt():
+    for i in range(num_of_rocks):
+        x_posi = i * (rWidth + extra_space_per_stone) 
+        screen.blit(rockImg, (x_posi, beltY))
+
+
 #Now we will start the Event/Game loop
 running = True
 #EventLoop
@@ -23,7 +47,9 @@ while running:
             running = False
             #sys.exit(): Removing this now, because this instantly kills the program
             #and we want to close the window first and then kill the program
-
+            
+    draw_belt()        
+    player(playerX, playerY)
     pygame.display.update()
 
 # Quit the game: Outside the event loop
